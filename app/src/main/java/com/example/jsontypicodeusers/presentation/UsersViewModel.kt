3f25a2b6004410bb0ccc88ab118170add20b365f
Kuru Juhani <juhani.kuru@lapinamk.fi>
2026-03-16 11:35:09 +0200

    - add internet permission - add getAllusers in viewmodel

--- app/src/main/java/com/example/jsontypicodeusers/presentation/UsersViewModel.kt
+++ app/src/main/java/com/example/jsontypicodeusers/presentation/UsersViewModel.kt
@@ -1,10 +1,47 @@
 package com.example.jsontypicodeusers.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jsontypicodeusers.domain.typiCodeService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class UsersViewModel : ViewModel() {
+
+
+
     private val _state = MutableStateFlow(UsersState())
     val state = _state.asStateFlow()
+
+
+    init {
+        getAllUsers()
+    }
+
+
+
+
+    fun getAllUsers() {
+        viewModelScope.launch {
+            try {
+
+                _state.update { currentState ->
+                    currentState.copy(loading = true)
+                }
+
+
+                val users = typiCodeService.getUsers()
+                _state.update { currentState ->
+                    currentState.copy(items = users)
+                }
+            } catch (e: Exception) {
+                _state.update { currentState -> currentState.copy(error = e.message) }
+            } finally {
+                _state.update { currentState -> currentState.copy(loading = false) }
+            }
+        }
+
+    }
 }

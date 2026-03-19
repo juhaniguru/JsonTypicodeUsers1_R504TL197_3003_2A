945c66c09e5e893c894cd7708e15a48667d71ebb
Kuru Juhani <juhani.kuru@lapinamk.fi>
2026-03-19 13:53:32 +0200

    - add navigationdrawer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
 
 
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
--- app/src/main/java/com/example/jsontypicodeusers/MainActivity.kt
+++ app/src/main/java/com/example/jsontypicodeusers/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jsontypicodeusers.presentation.UsersScreenRoot
 import com.example.jsontypicodeusers.ui.theme.JsonTypicodeUsersTheme
 
@@ -20,7 +32,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JsonTypicodeUsersTheme {
-                UsersScreenRoot()
+
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+
+                ModalNavigationDrawer(
+
+
+                    // tämä on drawerin sisältö
+                    drawerContent = {
+                        ModalDrawerSheet {
+                            Spacer(modifier = Modifier.height(16.dp))
+                            NavigationDrawerItem(
+                                label = {
+                                    Text(stringResource(R.string.users))
+                                },
+                                icon = {
+                                    Icon(
+                                        Icons.Default.Home,
+                                        contentDescription = stringResource(R.string.users)
+                                    )
+                                },
+                                onClick = {},
+                                selected = true
+                            )
+                        }
+                    }, drawerState = drawerState
+                ) {
+                    // Sisältö tähän
+                }
             }
         }
     }

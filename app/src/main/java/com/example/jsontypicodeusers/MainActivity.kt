e0c8d0b31753a2654d59574921e6fb443ecd2a96
Kuru Juhani <juhani.kuru@lapinamk.fi>
2026-03-19 13:55:49 +0200

    - add navhost and usersscreenroot

--- app/src/main/java/com/example/jsontypicodeusers/MainActivity.kt
+++ app/src/main/java/com/example/jsontypicodeusers/MainActivity.kt
@@ -23,6 +23,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.jsontypicodeusers.presentation.UsersScreenRoot
 import com.example.jsontypicodeusers.ui.theme.JsonTypicodeUsersTheme
 
@@ -34,7 +37,7 @@ class MainActivity : ComponentActivity() {
             JsonTypicodeUsersTheme {
 
                 val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-
+                val navController = rememberNavController()
 
                 ModalNavigationDrawer(
 
@@ -59,7 +62,16 @@ class MainActivity : ComponentActivity() {
                         }
                     }, drawerState = drawerState
                 ) {
-                    // Sisältö tähän
+                    NavHost(
+                        navController = navController,
+
+                        startDestination = "users",
+
+                        ) {
+                        composable("users") {
+                            UsersScreenRoot()
+                        }
+                    }
                 }
             }
         }
